0169b81153fd4da1caac159294f02254eb6b07c9
Ben Katzir <60402610+benkatzir@users.noreply.github.com>
2024-06-01 13:37:13 -0700

    Update morse_password_hinter.ino

--- morse_password_hinter.ino
+++ morse_password_hinter.ino
@@ -4,7 +4,22 @@
 #define LETTER_GAP 1500     // Duration between letters
 #define WORD_GAP 3000       // Duration between words
 
-const int ledPin = 13; // Built-in LED pin
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int ledPin = 13;    // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;       // variable for reading the pushbutton status
+int lastButtonState = 0;   // variable to store the previous button state
+unsigned long buttonPressTime = 0;  // variable to store the time when the button is pressed
+unsigned long buttonReleaseTime = 0;  // variable to store the time when the button is released
+
+String accountName = "";   // variable to store the account name
+String passwordMorse = ""; // variable to store the password in Morse code
+String passwordText = "";  // variable to store the password in text
+
+//String passwordsMorse[] = {}; // array to store passwords in Morse code
+int accountIndex = 0;      // index to keep track of the number of accounts
 
 String accounts[] = {"CitiBank", "Gmail", "Canvas", "Kaiser Permanente", "Amazon", "Spotify", "AppleID"};
 String passwords[] = {"hunnidbandz", "goGlGogl", "DeliaG", "koolaidPunch", "Bezos", "meus1c", "penpineapplepen"};
@@ -14,6 +29,7 @@ int selectedAccount = -1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600); // Start serial communication at 9600 baud
   displayInitialMenu();
 }
@@ -26,13 +42,15 @@ void displayInitialMenu() {
   Serial.println("Enter \"1\" or \"2\":");
   Serial.println("1. Type the password");
   Serial.println("2. Listen to half the password in Morse Code");
+  Serial.println("3. to create a password for a new account.");
 }
+
+int arraySize = sizeof(accounts) / sizeof(accounts[0]);
 
 // Function to display the account selection menu
 void displayAccountMenu() {
   Serial.println("PASSWORD RETRIEVER:");
   Serial.println("Enter the number of the account you need a password for:");
-  int arraySize = sizeof(accounts) / sizeof(accounts[0]);
   for (int i = 0; i < arraySize; i++) {
     Serial.print(i + 1);
     Serial.print(". ");
@@ -134,6 +152,9 @@ void checkPassword(int accountIndex, String inputPassword) {
 }
 
 void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     input.trim(); // Remove any leading/trailing whitespace
@@ -145,6 +166,71 @@ void loop() {
         Serial.print("YOU: ");
         Serial.println(accounts[input.toInt()-1]);
         displayAccountMenu();
+      } else if (selectedMode == 3) {
+        
+        
+        Serial.println("Write the name of an account to create a password for:");
+        while (Serial.available() == 0) {
+          // wait for user input
+        }
+        accountName = Serial.readStringUntil('\n');
+        accounts[accountIndex] = accountName;
+      
+        Serial.println("Type in Morse code password. Press '#' to end input.");
+        passwordMorse = "";  // reset password
+        passwordText = "";   // reset password text
+        while (true) {
+          buttonState = digitalRead(buttonPin); // update buttonState
+          if (buttonState == HIGH && lastButtonState == LOW) {
+            // button was just pressed
+            buttonPressTime = millis(); // record the time the button is pressed
+            digitalWrite(ledPin, HIGH); // turn LED on
+          } else if (buttonState == LOW && lastButtonState == HIGH) {
+            // button was just released
+            buttonReleaseTime = millis(); // record the time the button is released
+            digitalWrite(ledPin, LOW); // turn LED off
+
+            // calculate the duration of the button press
+            unsigned long pressDuration = buttonReleaseTime - buttonPressTime;
+
+            // classify the duration and add to passwordMorse
+            if (pressDuration < DOT_DURATION) {
+              passwordMorse += ".";
+            } else if (pressDuration < DASH_DURATION) {
+              passwordMorse += "-";
+            } else if (pressDuration < LETTER_GAP) {
+              passwordMorse += " ";
+            } else if (pressDuration < WORD_GAP) {
+              passwordMorse += " / ";
+            } else {
+              Serial.println("Unknown duration, try again.");
+            }
+          }
+
+          // Check for end of input
+          if (Serial.available() > 0) {
+            char endInput = Serial.read();
+            if (endInput == '#') {
+              // convert Morse to text
+              passwordText = morseToText(passwordMorse);
+              passwords[arraySize-1] = passwordText;
+              
+              Serial.print("Account: ");
+              Serial.println(accountName);
+              Serial.print("Password in Morse: ");
+              Serial.println(passwordMorse);
+              Serial.print("Password in text: ");
+              Serial.println(passwordText);
+
+              accountIndex++; // increment account index for the next account
+              break;
+            }
+          }
+
+          // save the current state as the last state, for the next loop iteration
+          lastButtonState = buttonState;
+        }
+      
       } else {
         Serial.print("YOU: ");
         Serial.println(accounts[input.toInt()-1]);
@@ -191,4 +277,43 @@ void loop() {
       mode = 0; // Reset mode for next cycle
     }
   }
+}
+
+// Function to convert Morse code to text
+String morseToText(String morse) {
+  // Define Morse code dictionary
+  const char* morseDictionary[][2] = {
+    {".-", "A"}, {"-...", "B"}, {"-.-.", "C"}, {"-..", "D"}, {".", "E"},
+    {"..-.", "F"}, {"--.", "G"}, {"....", "H"}, {"..", "I"}, {".---", "J"},
+    {"-.-", "K"}, {".-..", "L"}, {"--", "M"}, {"-.", "N"}, {"---", "O"},
+    {".--.", "P"}, {"--.-", "Q"}, {".-.", "R"}, {"...", "S"}, {"-", "T"},
+    {"..-", "U"}, {"...-", "V"}, {".--", "W"}, {"-..-", "X"}, {"-.--", "Y"},
+    {"--..", "Z"}, {"-----", "0"}, {".----", "1"}, {"..---", "2"}, {"...--", "3"},
+    {"....-", "4"}, {".....", "5"}, {"-....", "6"}, {"--...", "7"}, {"---..", "8"},
+    {"----.", "9"}
+  };
+  int dictionarySize = sizeof(morseDictionary) / sizeof(morseDictionary[0]);
+  
+  String result = "";
+  String currentSymbol = "";
+  morse += " "; // add a space at the end to capture the last symbol
+
+  for (int i = 0; i < morse.length(); i++) {
+    if (morse[i] == ' ') {
+      if (currentSymbol != "") {
+        // find the symbol in the dictionary
+        for (int j = 0; j < dictionarySize; j++) {
+          if (currentSymbol == morseDictionary[j][0]) {
+            result += morseDictionary[j][1];
+            break;
+          }
+        }
+        currentSymbol = "";
+      }
+    } else {
+      currentSymbol += morse[i];
+    }
+  }
+  
+  return result;
 }
